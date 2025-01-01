2d5c7532df1df15f2f3cef3b0e66a29014a56258
Kevan Tamboli <kevantamboli@gmail.com>
2025-01-01 13:59:35 +0530

    Add dual tank support with separate controls and images

--- tank.py
+++ tank.py
@@ -3,20 +3,34 @@ class Tank:
     def __init__(self, game):
         self.screen = game.display
         self.screen_rect = game.display.get_rect()
-        self.original_image = pygame.image.load('images/tankbody.bmp')
-        self.image = pygame.transform.scale(self.original_image, (70,70))
-        self.rect = self.image.get_rect()
-        self.rect.midleft = self.screen_rect.midleft
+        self.original_image1 = pygame.image.load('Tank-Duel/images/tankbody.bmp')
+        self.original_image2 = pygame.image.load('Tank-Duel/images/tankbody2.bmp')
+        self.image1 = pygame.transform.scale(self.original_image1, (70,70))
+        self.image2 = pygame.transform.scale(self.original_image2, (70,70))
+        self.rect1 = self.image1.get_rect()
+        self.rect1.midleft = self.screen_rect.midleft
+        self.rect2 = self.image2.get_rect()
+        self.rect2.midright = self.screen_rect.midright
         self.moving_up = False
         self.moving_down = False
     
-    def blitme(self):
-        self.screen.blit(self.image, self.rect)
+    def blitme(self,playerNo):
+        if playerNo == 1:
+            self.screen.blit(self.image1, self.rect1)
+        else:
+            self.screen.blit(self.image2, self.rect2)
+        # self.screen.blit(self.image1, self.rect)
 
     def update(self):
         if self.moving_up:
-            if self.rect.y > 0:
-                self.rect.y -= 10
+            if self.rect1.y > 0:
+                self.rect1.y -= 10
         elif self.moving_down:
-            if self.rect.bottom < self.screen_rect.bottom:
-                self.rect.y += 10
+            if self.rect1.bottom < self.screen_rect.bottom:
+                self.rect1.y += 10
+        if self.moving_up:
+            if self.rect2.y > 0:
+                self.rect2.y -= 10
+        elif self.moving_down:
+            if self.rect2.bottom < self.screen_rect.bottom:
+                self.rect2.y += 10
--- tank_duels.py
+++ tank_duels.py
@@ -9,7 +9,8 @@ class Tank_duel:
         self.settings = Settings()
         self.display = pygame.display.set_mode((self.settings.screen_height, self.settings.screen_width))
         pygame.display.set_caption("tank_duels")
-        self.tank = Tank(self)
+        self.tank1 = Tank(self)
+        self.tank2 = Tank(self)
         self.clock = pygame.time.Clock()
 
 
@@ -17,14 +18,16 @@ class Tank_duel:
         """main loop of the game"""
         while True:
             self._check_events()
-            self.tank.update()
+            self.tank1.update()
+            self.tank2.update()
             self._update_screen()
             self.clock.tick(60)
     
 
     def _update_screen(self):
         self.display.fill(self.settings.bg_color)
-        self.tank.blitme()
+        self.tank1.blitme(1)
+        self.tank2.blitme(2)
         # holds the last drawn image on display
         pygame.display.flip()
 
@@ -35,16 +38,33 @@ class Tank_duel:
                     sys.exit()
                 elif event.type == pygame.KEYDOWN:
                     if event.key == pygame.K_UP:
-                        self.tank.moving_up = True
+                        self.tank1.moving_up = True
+                    if event.key == pygame.K_DOWN:
+                        self.tank1.moving_down = True
+                    # p1 controls
+                    if event.key == pygame.K_w:
+                        self.tank2.moving_up = True
+                    if event.key == pygame.K_s:
+                        self.tank2.moving_down = True
+                    # p2 controls
                 elif event.type == pygame.KEYUP:
                     if event.key == pygame.K_UP:
-                        self.tank.moving_up = False
-                if event.type == pygame.KEYDOWN:
+                        self.tank1.moving_up = False
                     if event.key == pygame.K_DOWN:
-                        self.tank.moving_down = True
-                elif event.type == pygame.KEYUP:
-                    if event.key == pygame.K_DOWN:
-                        self.tank.moving_down = False
+                        self.tank1.moving_down = False
+                    # p1 controls
+                    if event.key == pygame.K_w:
+                        self.tank2.moving_up = False
+                    if event.key == pygame.K_s:
+                        self.tank2.moving_down = False
+                    # p2 controls
+                    
+                # if event.type == pygame.KEYDOWN:
+                #     if event.key == pygame.K_DOWN:
+                #         self.tank.moving_down = True
+                # elif event.type == pygame.KEYUP:
+                #     if event.key == pygame.K_DOWN:
+                #         self.tank.moving_down = False
                 
 
 if __name__ == "__main__":
